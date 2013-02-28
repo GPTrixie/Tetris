3b6396ba7e8aa4317f6e97fb292696ea3b94bb17
AlexDu76 <lucasduchesne@hotmail.com>
2013-02-28 10:28:54 +0100

    Signed-off-by: AlexDu76 <lucasduchesne@hotmail.com>

--- Tetris/partie.cs
+++ Tetris/partie.cs
@@ -20,8 +20,8 @@ namespace Tetris
             temp = 0;
             niveau = 0;
             this.musique = musique;
-            puit = new espace(); // taille de l'écrand de jeux
-            pieceSuivante = new espace(); // taille des carré des pieces
+            puit = new espace(22,11); // taille de l'écrand de jeux
+            pieceSuivante = new espace(4,4); // taille des carré des pieces
 
         }
         public int Score
