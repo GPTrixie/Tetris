5f688355c667466f1ff37b30b9cb72fbd30d6809
AlexDu76 <lucasduchesne@hotmail.com>
2013-03-01 16:30:57 +0100

    Signed-off-by: AlexDu76 <lucasduchesne@hotmail.com>

--- Tetris/espace.cs
+++ Tetris/espace.cs
@@ -90,6 +90,164 @@ namespace Tetris
             }
 
         }
+        public int testDeplacementPiece(int sens) //renvoie l'entier d'entre si possible de bouger autrement envoie 0 (1 droite, 2 gauche 3 bas)
+        {
+            @case[][] temp = pieceEnCour.getTableau();
+            if (sens == 1)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (j + 1 > 3)
+                        {
+                            if (temp[i][j].Pleine == 1)
+                            {
+                                if (j + pieceEnCour.getCentre().Y + 1 < largeur)
+                                {
+                                    if (tableau[i + pieceEnCour.getCentre().X][j + pieceEnCour.getCentre().Y + 1].Pleine == 1)
+                                    {
+
+                                        return 0;
+
+                                    }
+                                }
+                                else
+                                {
+                                    return 0;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (temp[i][j].Pleine == 1 && temp[i][j + 1].Pleine != 1)
+                            {
+                                if (j + pieceEnCour.getCentre().Y + 1 < largeur)
+                                {
+                                    if (tableau[i + pieceEnCour.getCentre().X][j + pieceEnCour.getCentre().Y + 1].Pleine == 1)
+                                    {
+
+                                        return 0;
+
+                                    }
+                                }
+                                else
+                                {
+                                    return 0;
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+                return 1;
+            }
+            if (sens == 2)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if (temp[i][j].Pleine == 1)
+                            {
+                                if (j + pieceEnCour.getCentre().Y != 0)
+                                {
+                                    if (tableau[i + pieceEnCour.getCentre().X][j + pieceEnCour.getCentre().Y - 1].Pleine == 1)
+                                    {
+
+                                        return 0;
+
+                                    }
+                                }
+                                else
+                                {
+                                    return 0;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (temp[i][j].Pleine == 1 && temp[i][j - 1].Pleine != 1)
+                            {
+                                if (j + pieceEnCour.getCentre().Y != 0)
+                                {
+                                    if (tableau[i + pieceEnCour.getCentre().X][j + pieceEnCour.getCentre().Y - 1].Pleine == 1)
+                                    {
+
+                                        return 0;
+
+                                    }
+                                }
+                                else
+                                {
+                                    return 0;
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+                return 2;
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (i == 4)
+                        {
+                            if (temp[i][j].Pleine == 1)
+                            {
+                                if (i + pieceEnCour.getCentre().X != hauteur - 1)
+                                {
+                                    if (tableau[i + pieceEnCour.getCentre().X + 1][j + pieceEnCour.getCentre().Y].Pleine == 1)
+                                    {
+
+                                        return 0;
+
+                                    }
+                                }
+                                else
+                                {
+                                    return 0;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (temp[i][j].Pleine == 1 && temp[i + 1][j].Pleine != 1)
+                            {
+                                if (j + pieceEnCour.getCentre().Y != hauteur - 1)
+                                {
+                                    if (tableau[i + pieceEnCour.getCentre().X + 1][j + pieceEnCour.getCentre().Y].Pleine == 1)
+                                    {
+
+                                        return 0;
+
+                                    }
+                                }
+                                else
+                                {
+                                    return 0;
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+                return 3;
+            }
+
+
+        }
+
+
         public void deplacerPiece(int x, int y)
         {
             @case centre = new @case(x + pieceEnCour.getCentre().X, y + pieceEnCour.getCentre().Y);
